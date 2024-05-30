038246b7d1bd1527b4ef702fa6c0ec30f88ed669
prabakaran <prabakaran@sodexis.com>
2024-05-30 20:05:39 +0530

    owl orm tutorials

---
+++ owl/static/src/components/todo_list/todo_list.js
@@ -0,0 +1,61 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+const { Component ,useState, onWillStart, useRef} = owl;
+
+export class OwlTodoList extends Component {
+    setup(){
+        this.state = useState({
+            task:{name:"", color:"#FF0000",completed:false},
+            taskList:[],
+            isEdit:false,
+            activeId: false,
+        })
+        this.orm = useService("orm")
+        this.model = 'owl.todo.list'
+        this.searchInput = useRef('search-input')
+        onWillStart(async ()=>{
+            await this.getAllTasks()
+            
+        })
+    }
+    async getAllTasks(){
+        this.state.taskList = await this.orm.searchRead(this.model, [], ['name','color','completed'])
+    }
+    async addTask(){
+        this.resetForm()
+        this.state.activeId = false
+        this.state.isEdit = false
+
+    }
+    editTask(task){
+        this.state.activeId = task.id
+        this.state.isEdit = true
+        this.state.task = {...task}
+    }
+    async saveTask(){
+        if (!this.state.isEdit){
+            await this.orm.create(this.model, [this.state.task])
+        } else{
+            await this.orm.write(this.model, [this.state.activeId], this.state.task)
+        }
+        await this.getAllTasks()
+    }
+    async deleteTask(task){
+        await this.orm.unlink(this.model,[task.id])
+        await this.getAllTasks()
+    }
+    resetForm(){
+        this.state.task = {name:"", color:"#FF0000",completed:false}
+        this.state.activeId = false
+        this.state.isEdit = false
+    }
+    async searchTasks(){
+        const text =this.searchInput.el.value
+        this.state.taskList = await this.orm.searchRead(this.model, [['name','ilike',text]], ['name','color','completed'])
+    }
+
+}
+OwlTodoList.template = 'owl.todolist'
+registry.category('actions').add('owl.action_owl_todo_list_js', OwlTodoList);
